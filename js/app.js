fdbda93c4aecaee3730908adf2810f1e56799c48
brianchen322 <brian.chen322@outlook.com>
2015-12-21 16:31:26 +0100

    Comment Js et Css

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 
 $(document).ready(function(){
+
+	// Cool des commentaires :)
+	// N'hésites pas écrire des commentaires plus globaux
 	$('form').on('submit',function(event){ //prend mon form et au submit tu executes la fonction
 		event.preventDefault(); // dans la fonction tu prends le premier evenement. tu evites son comportement par default
 
 		var aAjouter =$('input[name=newelements]').val();
 		$('input[name=newelements]').val("");
-		$('.applicationToDoList').append('<div class="item"><input type="checkbox" name="elementsliste" id="checkboxID"/><button class="close">X</button>' + aAjouter + '</div>');
 
+		// Découpe en plus petits lots.
+		// Cette ligne est trop longue et devient illisible.
+		var newElmt = '<div class="item">' 
+			+ '<input type="checkbox" name="elementsliste" id="checkboxID"/>'
+			+ '<button class="close">X</button>' + aAjouter + '</div>');
+
+		$('.applicationToDoList').append(newElmt);
 		calcul();
 	});
 
+	
 	$('.applicationToDoList').on('click','#checkboxID',function(){
 		$(this).parent('.item').toggleClass('barre');
 
